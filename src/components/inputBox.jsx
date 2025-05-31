99cad1ed326c8987b979f2f8c76610e2feeeb7ab
Ashish Kumar Atul <162558245+Ashish-Kumar-Atul@users.noreply.github.com>
2025-06-01 04:42:16 +0530

    Components and hooks modified

--- src/components/inputBox.jsx
+++ src/components/inputBox.jsx
@@ -4,12 +4,11 @@ function InputBox({
     label,
     amount,
     onAmountChange,
-    currency,
     onCurrencyChange,
     currencyOptions = [],
     selectCurrency = "usd",
     amountDisable = false,
-    currescyDisable = false,
+    currencyDisable = false,
     className = "",
 }) {
    const amountInputID = useId()
@@ -34,12 +33,16 @@ function InputBox({
                 <p className="text-black/40 mb-2 w-full">Currency Type</p>
                 <select
                     className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
-                    
+                    value={selectCurrency}
+                    onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+                    disabled={currencyDisable}
                 >
-                    
-                        <option value="usd">
-                            usd
+                    {currencyOptions.map((currency) => (
+                        <option key={currency} value={currency}>
+                            {currency}
                         </option>
+                    ))}
+                        
                 
                 </select>
             </div>
